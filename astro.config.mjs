47f26cbcd9623fbb097743b7771a6bfd76adf228
Scott Rassbach <srassbac@utk.edu>
2024-09-12 13:48:28 -0400

    End of tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://gorgeous-vacherin-71a74b.netlify.app/"
+  site: "https://gorgeous-vacherin-71a74b.netlify.app/",
+  integrations: [preact()]
 });
